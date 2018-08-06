1a4189d07bf9e20af5dc44ddf7b5fb689267e2f7
Palash Mondal <palashm@live.in>
2018-08-06 11:42:14 +0530

    Progress

--- 01-simple-product-list-app/src/components/SearchBar.js
+++ 01-simple-product-list-app/src/components/SearchBar.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   render() {
     return (
-      <div class="content-header">
-        <div class="content-info">
+      <div className="content-header">
+        <div className="content-info">
           <form>
             <input
               type="text"
