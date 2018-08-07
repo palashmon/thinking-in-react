d146739bb6157bb415857ec6a359112d2b1b5a33
Palash Mondal <palashm@live.in>
2018-08-07 20:52:52 +0530

    Step 1 * Break The UI Into A Component Hierarchy

--- src/api.js
+++ src/api.js
@@ -35,8 +35,33 @@ const JsonApi = {
     },
     { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
   ],
+  cards: [
+    {
+      title: 'Card Title 1',
+      text: 'Laudantium enim quasi est quidem magnam voluptate ipsam eos',
+      src: 'https://picsum.photos/200/300?image=1067'
+    },
+    {
+      title: 'Card Title 2',
+      text: 'Oluptatem occaecati omnis eligendi aut ad oluptatem doloribus vel accusantium quis pariatur',
+      src: 'https://picsum.photos/200/300?image=1059'
+    },
+    {
+      title: 'Card Title 3',
+      text: 'Ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa',
+      src: 'https://picsum.photos/200/300?image=1050'
+    },
+    {
+      title: 'Card Title 4',
+      text: 'Repudiandae veniam quaerat sunt sed lias aut fugiat sit autem sed est',
+      src: 'https://picsum.photos/200/300?image=1015'
+    }
+  ],
   getAllProducts: function() {
     return this.products;
+  },
+  getAllCards: function() {
+    return this.cards;
   }
 };
 
